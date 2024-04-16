baa38a3cbe9844080758e8d7dd3d986fa1b1deef
HEITOR RICHARD <heitorrichard48@gmail.com>
2024-04-15 22:19:00 -0300

    Atualizando a aba de cadastro e de contato

--- StoreApp/views.py
+++ StoreApp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from StoreApp.models import Departamento, Produto
-from StoreApp.forms import CadastroForm
+from StoreApp.forms import CadastroForm, ContatoForm
 
 # Create your views here.
 def index(request):
@@ -43,9 +43,36 @@ def sobre_empresa(request):
 
 def cadastro(request):
 
-    formulario = CadastroForm
+    #armazena mensagem de sucesso ou erro
+    mensagem = '' 
+
+    #se o formulário foi submetido
+    if request.method == 'POST':
+        formulario = CadastroForm(request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            formulario = CadastroForm()
+            mensagem = "Cliente cadastrado com sucesso "
+        else:
+            mensagem = "Verifique os erros abaixo: "
+    else: 
+        #se o formulário não foi submetido. Entrei na page pelo menu.
+        #e o form deve vir vazio.
+        formulario = CadastroForm()
 
     context = {
-        'formulario_cadastro' : formulario
+        'formulario_cadastro' : formulario,
+        'mensagem' : mensagem
     }
     return render(request, 'cadastro.html', context)
+
+def contato(request):
+    mensagem = ''
+    formulario = ContatoForm()
+
+    context = {
+        'mensagem' : mensagem,
+        'formulario_contato' : formulario
+    }
+    
+    return render(request, 'contato.html', context)
